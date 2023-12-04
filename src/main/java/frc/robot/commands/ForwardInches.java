77d484b7f75c733d4c2305f2a8e12d6fcfc1fe68
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2023-12-04 15:18:40 -0600

    Create ForwardInches.java

---
+++ src/main/java/frc/robot/commands/ForwardInches.java
@@ -0,0 +1,38 @@
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ForwardInches extends CommandBase {
+  Drive drive;
+  private double distGoal;
+  private double leftEncoderDist;
+  private double leftEncoderOffsetAsRotations;
+  private double rightEncoderDist;
+  private double rightEncoderOffsetAsRotations;
+  
+  public ForwardInches(Drive drive, double dist) {
+    addRequirements(drive);
+    this.drive = drive;
+    distGoal = dist;
+  }
+
+  public void initialize() {
+    drive.tankDrive(0, 0);
+    leftEncoderOffsetAsRotations = drive.getLeftEncoderPos();
+    rightEncoderOffsetAsRotations = drive.getRightEncoderPos();
+    leftEncoderDist = 0;
+    rightEncoderDist = 0;
+  }
+  
+  public void execute() {
+    drive.tankDrive(0.5, 0.5);
+    leftEncoderDist = Constants.AutonConstants.kWheelCircumferenceInch * (drive.getLeftEncoderPos() - leftEncoderOffsetAsRotations);
+    rightEncoderDist = Constants.AutonConstants.kWheelCircumferenceInch * (drive.getRightEncoderPos() - rightEncoderOffsetAsRotations);
+  }
+
+  public void end() {
+    drive.tankDrive(0, 0);
+  }
+  
+  public boolean isFinished() {
+    return (leftEncoderDist - rightEncoderDist) / 2 >= distGoal;
+  }
+}
