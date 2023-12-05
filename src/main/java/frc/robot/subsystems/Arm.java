78beac43ede6a379fc7d55095dd689765d6465ac
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2023-12-05 08:28:49 -0600

    Added empty methods for things to maybe do for Arm.java

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,7 +1,23 @@
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Arm {
+public class Arm extends SubsystemBase {
   public Arm() {
+    
+  }
+
+  public void score() {
+    
+  }
+  public void retract() {
+    
+  }
+  public void down() {
+    
+  }
+  public void toAngle(double degrees) {
+    
+  }
+  public void setSpeed(double speed) {
     
   }
 }
