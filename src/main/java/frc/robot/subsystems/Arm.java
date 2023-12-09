86a7d03d3f03a7df82bdf79e89d402eea9e801a3
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2023-12-09 08:22:11 -0600

    Numbers in Arm based on constants

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -2,17 +2,18 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import frc.robot.Constants.ArmConstants;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
-  private VictorSPX armMotor = new VictorSPX(6);
+  private VictorSPX armMotor = new VictorSPX(ArmConstants.kArmCanId);
 
   public Arm() {
   }
 
   public void setSpeed(double speed) {
-    armMotor.set(ControlMode.PercentOutput, 0.23 * speed);
+    armMotor.set(ControlMode.PercentOutput, ArmConstants.kVelocityPercentMultiplier * speed);
   }
 
   public void armTriggers() {
